535490774965a411c346b0bb862839ceaaf0afb3
D2Kumo <djibril.samassa@eemi.com>
2023-12-16 15:31:58 +0100

    menu burger

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,16 +10,33 @@ import {
   MenuItem,
   Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Style from "./Header.module.css";
 import SaveAltIcon from "@mui/icons-material/SaveAlt";
 import Person2Icon from "@mui/icons-material/Person2";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Header() {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [menuAnchorEl, setMenuAnchorEl] = useState(null);
   const [openMenu, setOpenMenu] = useState(Boolean(anchorEl));
   const [menuToDisplay, setMenuToDisplay] = useState();
   const [selected, setSelected] = useState(0);
+  const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
+  const [windowSize, setWindowSize] = useState();
+
+  useEffect(() => {
+    setWindowSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleResize = (e) => {
+    setWindowSize(e.currentTarget.innerWidth);
+  };
 
   const sections = [
     { id: 0, name: "Formations" },
@@ -28,7 +45,6 @@ export default function Header() {
     { id: 3, name: "A propos de moi" },
     { id: 4, name: "Livre blanc" },
   ];
-
   const downloadMenuItem = [
     {
       type: "download",
@@ -79,95 +95,136 @@ export default function Header() {
     setOpenMenu(false);
   };
 
+  const content = (
+    <span
+      style={{
+        width: `${windowSize > 1200 ? "60vw" : "fit-content"}`,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: `${windowSize > 1200 ? "row" : "column"}`,
+      }}
+    >
+      <List
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexDirection: `${windowSize > 1200 ? "row" : "column"}`,
+        }}
+      >
+        {sections.map((s) => {
+          return (
+            <span key={s.id}>
+              <ListItem disablePadding>
+                <ListItemButton
+                  sx={{ textAlign: "center" }}
+                  onClick={() => {
+                    setSelected(s.id);
+                  }}
+                >
+                  <ListItemText primary={s.name} />
+                </ListItemButton>
+              </ListItem>
+              {selected === s.id ? (
+                <Divider className={Style.selected} />
+              ) : null}
+            </span>
+          );
+        })}
+      </List>
+
+      <span
+        style={{
+          display: "flex",
+          flexDirection: `${windowSize > 1200 ? "row" : "column"}`,
+        }}
+      >
+        <button
+          className={Style.customButton}
+          id="fade-button"
+          aria-controls={openMenu ? "fade-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={openMenu ? "true" : undefined}
+          onClick={(e) => {
+            handleClick(e, "download");
+          }}
+        >
+          <SaveAltIcon />
+        </button>
+        <button
+          className={Style.customButton}
+          id="fade-button"
+          aria-controls={openMenu ? "fade-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={openMenu ? "true" : undefined}
+          onClick={(e) => {
+            handleClick(e, "contact");
+          }}
+        >
+          <Person2Icon />
+        </button>
+      </span>
+
+      <Menu
+        anchorEl={anchorEl}
+        id="fade-menu"
+        MenuListProps={{
+          "aria-labelledby": "fade-button",
+        }}
+        open={openMenu}
+        onClose={() => {
+          handleClose();
+        }}
+      >
+        {menuToDisplay?.map((item) => (
+          <MenuItem
+            onClick={() => {
+              handleClose();
+            }}
+            key={item.id}
+          >
+            <a
+              download={item.type === "download" ? item.download : null}
+              href={item.href}
+            >
+              {" "}
+              {item.title}
+            </a>
+          </MenuItem>
+        ))}
+      </Menu>
+    </span>
+  );
+
   return (
     <nav>
       <Box className={Style.container}>
         <h2 className={Style.logo}>Djibril Samassa</h2>
 
-        <List
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          {sections.map((s) => {
-            return (
-              <span key={s.id}>
-                <ListItem disablePadding>
-                  <ListItemButton
-                    sx={{ textAlign: "center" }}
-                    onClick={() => {
-                      setSelected(s.id);
-                    }}
-                  >
-                    <ListItemText primary={s.name} />
-                  </ListItemButton>
-                </ListItem>
-                {selected === s.id ? (
-                  <Divider className={Style.selected} />
-                ) : null}
-              </span>
-            );
-          })}
-        </List>
-
-        <span>
-          <button
-            className={Style.customButton}
-            id="fade-button"
-            aria-controls={openMenu ? "fade-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={openMenu ? "true" : undefined}
+        {windowSize > 1200 ? (
+          <span> {content}</span>
+        ) : (
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
             onClick={(e) => {
-              handleClick(e, "download");
+              setBurgerMenuOpen(true);
+              setMenuAnchorEl(e.currentTarget);
             }}
           >
-            <SaveAltIcon />
-          </button>
-          <button
-            className={Style.customButton}
-            id="fade-button"
-            aria-controls={openMenu ? "fade-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={openMenu ? "true" : undefined}
-            onClick={(e) => {
-              handleClick(e, "contact");
-            }}
-          >
-            <Person2Icon />
-          </button>
-        </span>
-
-        <Menu
-          anchorEl={anchorEl}
-          id="fade-menu"
-          MenuListProps={{
-            "aria-labelledby": "fade-button",
-          }}
-          open={openMenu}
-          onClose={() => {
-            handleClose();
-          }}
-        >
-          {menuToDisplay?.map((item) => (
-              <MenuItem
-                onClick={() => {
-                  handleClose();
-                }}
-                key={item.id}
-              >
-                <a
-                  download={item.type === "download" ? item.download : null}
-                  href={item.href}
-                >
-                  {" "}
-                  {item.title}
-                </a>
-              </MenuItem>
-            ))}
-        </Menu>
+            <MenuIcon />
+          </IconButton>
+        )}
       </Box>
+      <Menu
+        anchorEl={menuAnchorEl}
+        open={burgerMenuOpen}
+        onClose={() => setBurgerMenuOpen(false)}
+      >
+        {content}
+      </Menu>
     </nav>
   );
 }
